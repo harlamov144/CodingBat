b81e4028af2ddab32940237b9b9ef4514e724708
harlamov144 <harlamov144@yandex.ru>
2020-07-20 21:39:41 +0300

    String1
     - makeTags

---
+++ src/com/codingbat/string1/makeTags.java
@@ -0,0 +1,21 @@
+package com.codingbat.string1;
+
+/**
+ * The web is built with HTML strings like "<i>Yay</i>" which draws Yay as italic text. In this example, the "i"
+ * tag makes <i> and </i> which surround the word "Yay". Given tag and word strings, create the HTML string with
+ * tags around the word, e.g. "<i>Yay</i>".
+ *
+ * makeTags("i", "Yay") → "<i>Yay</i>"
+ * makeTags("i", "Hello") → "<i>Hello</i>"
+ * makeTags("cite", "Yay") → "<cite>Yay</cite>"
+ */
+public class makeTags {
+    public String makeTags(String tag, String word) {
+        StringBuilder str = new StringBuilder("<");
+        return str.append(tag).append(">").append(word).append("</").append(tag).append(">").toString();
+    }
+
+    public String makeTagsOldSolution(String tag, String word) {
+        return "<" + tag + ">" + word + "</" + tag + ">";
+    }
+}
