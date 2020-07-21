9d1f92e2ea8c3c7479045dc45f5d907c50a9a65b
harlamov144 <harlamov144@yandex.ru>
2020-07-21 21:58:00 +0300

    String1
     - endsLy

---
+++ src/com/codingbat/string1/endsLy.java
@@ -0,0 +1,14 @@
+package com.codingbat.string1;
+
+/**
+ * Given a string, return true if it ends in "ly".
+ *
+ * endsLy("oddly") → true
+ * endsLy("y") → false
+ * endsLy("oddy") → false
+ */
+public class endsLy {
+    public boolean endsLy(String str) {
+        return str.endsWith("ly");
+    }
+}
