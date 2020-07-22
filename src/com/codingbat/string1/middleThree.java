ec61ecfdc65dc7def328445ded7faa01a7dfb78e
harlamov144 <harlamov144@yandex.ru>
2020-07-22 22:14:44 +0300

    String1
     - middleThree

---
+++ src/com/codingbat/string1/middleThree.java
@@ -0,0 +1,15 @@
+package com.codingbat.string1;
+
+/**
+ * Given a string of odd length, return the string length 3 from its middle, so "Candy" yields "and".
+ * The string length will be at least 3.
+ *
+ * middleThree("Candy") → "and"
+ * middleThree("and") → "and"
+ * middleThree("solving") → "lvi"
+ */
+public class middleThree {
+    public String middleThree(String str) {
+        return str.substring(str.length()/2 - 1, str.length()/2 + 2);
+    }
+}
