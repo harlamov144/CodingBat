27f32d315302465a9e6c1cf93d1b3c7e3e953890
harlamov144 <harlamov144@yandex.ru>
2020-08-05 22:14:24 +0300

    String1
     - withoutX

---
+++ src/com/codingbat/string1/withoutX.java
@@ -0,0 +1,32 @@
+package com.codingbat.string1;
+
+/**
+ * Given a string, if the first or last chars are 'x', return the string without those 'x' chars,
+ * and otherwise return the string unchanged.
+ *
+ * withoutX("xHix") → "Hi"
+ * withoutX("xHi") → "Hi"
+ * withoutX("Hxix") → "Hxi"
+ */
+public class withoutX {
+    public String withoutX(String str) {
+        if(str.startsWith("x")) str = str.substring(1);
+        if(str.endsWith("x")) str = str.substring(0, str.length()-1);
+        return str;
+    }
+
+    public String withoutXOldSolution(String str) {
+
+        if(str.startsWith("x")){
+            str = str.substring(1, str.length());
+        }
+
+        if(str.endsWith("x")){
+            str = str.substring(0, str.length()-1);
+        }
+
+        return str;
+    }
+
+
+}
