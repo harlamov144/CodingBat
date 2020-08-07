93641977beaf2b63e5997332d371769e71a7effc
harlamov144 <harlamov144@yandex.ru>
2020-08-07 21:02:48 +0300

    Logic1
     - old35

---
+++ src/com/codingbat/logic1/old35.java
@@ -0,0 +1,23 @@
+package com.codingbat.logic1;
+
+/**
+ * Return true if the given non-negative number is a multiple of 3 or 5, but not both. Use the % "mod"
+ * operator -- see Introduction to Mod
+ *
+ * old35(3) → true
+ * old35(10) → true
+ * old35(15) → false
+ */
+public class old35 {
+    public boolean old35(int n) {
+        return (n%3 == 0 && n%5 != 0) || (n%3 != 0 && n%5 == 0);
+    }
+
+    public boolean old35OldSolution(int n) {
+        int a = n%3;
+        int b = n%5;
+
+        if( (a==0 || b == 0) && a != b  ) return true;
+        return false;
+    }
+}
