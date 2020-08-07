6b0b00050f1f62b8adc7752b479562f84054ec00
harlamov144 <harlamov144@yandex.ru>
2020-08-07 21:08:56 +0300

    Logic1
     - nearTen

---
+++ src/com/codingbat/logic1/nearTen.java
@@ -0,0 +1,22 @@
+package com.codingbat.logic1;
+
+/**
+ * Given a non-negative number "num", return true if num is within 2 of a multiple of 10. Note: (a % b) is the
+ * remainder of dividing a by b, so (7 % 5) is 2. See also: Introduction to Mod
+ *
+ * nearTen(12) → true
+ * nearTen(17) → false
+ * nearTen(19) → true
+ */
+public class nearTen {
+    public boolean nearTen(int num) {
+        return num%10 <= 2 || num%10 >= 8;
+    }
+
+    public boolean nearTenOldSolution(int num) {
+        int res = num%10;
+        if(res <= 2  || res >= 8) return true;
+        return false;
+    }
+
+}
