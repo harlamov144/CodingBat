acd6452affb835d2a50c4f9bb8796149a9af2afa
harlamov144 <harlamov144@yandex.ru>
2020-08-12 21:50:59 +0300

    Array1
     - makePi

---
+++ src/com/codingbat/array1/makePi.java
@@ -0,0 +1,18 @@
+package com.codingbat.array1;
+
+/**
+ * Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+ *
+ * makePi() → [3, 1, 4]
+ */
+public class makePi {
+    public int[] makePi() {
+        return new int[]{3, 1, 4};
+    }
+
+    public int[] makePiOldSolution() {
+        int[] mas = {3, 1, 4};
+
+        return mas;
+    }
+}
